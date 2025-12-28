9bf0886948e143c20d752ac557bdb4e1367a6799
zmiller2025 <zerotraceprocleaning@gmail.com>
2025-12-28 14:08:13 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -5,3 +5,10 @@ import ZeroTraceProCleaningSiteDraft from "./ZeroTraceSite";
 export default function Page() {
   return <ZeroTraceProCleaningSiteDraft />;
 }
+<Image
+  src="/logo.png"
+  alt="Zero Trace Pro Cleaning Logo"
+  width={48}
+  height={48}
+  className="rounded-xl"
+/>
