9aed2ffe1599739f985ad9505d3a8ceff51b233e
zmiller2025 <zerotraceprocleaning@gmail.com>
2025-12-28 13:59:58 -0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,5 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1 style={{ fontSize: 48 }}>🚨 PAGE.JSX IS WORKING</h1>
-      <p>If you see this, the old page is gone.</p>
-    </div>
-  );
+import ZeroTraceProCleaningSiteDraft from "./ZeroTraceSite";
+
+export default function Page() {
+  return <ZeroTraceProCleaningSiteDraft />;
 }
